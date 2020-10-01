a0eba382924d308bb2a50ccf1aecb118a50f9caa
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-10-01 23:45:38 +0200

    Added UnitTest for parsing string(s).

--- TestPythonCodeAnalyzer/TestParser.cs
+++ TestPythonCodeAnalyzer/TestParser.cs
@@ -53,7 +53,6 @@ namespace TestPythonCodeAnalyzer
             Assert.Equal(Token.TokenKind.Name, node.Name.Kind );
             Assert.Equal(0UL, node.Start );
             Assert.Equal(5UL, node.End );
-            Assert.Equal(Token.TokenKind.Name, node.Name.Kind );
             Assert.Equal(0UL, node.Name.Start );
             Assert.Equal(5UL, node.Name.End );
         }
@@ -68,9 +67,105 @@ namespace TestPythonCodeAnalyzer
             Assert.Equal(Token.TokenKind.Number, node.Number.Kind );
             Assert.Equal(0UL, node.Start );
             Assert.Equal(5UL, node.End );
-            Assert.Equal(Token.TokenKind.Number, node.Number.Kind );
             Assert.Equal(0UL, node.Number.Start );
             Assert.Equal(5UL, node.Number.End );
+        }
+        
+        [Fact]
+        public void TestElipsisAtom()
+        {
+            var parser = Setup( new Token[] { new Token(0, 5, Token.TokenKind.PyElipsis ), new Token(5, 5, Token.TokenKind.EOF) } );
+            
+            Assert.True(parser != null);
+            ElipsisLiteralExpression node = (ElipsisLiteralExpression)parser.ParseAtom();
+            Assert.Equal(Token.TokenKind.PyElipsis, node.Elipsis.Kind );
+            Assert.Equal(0UL, node.Start );
+            Assert.Equal(5UL, node.End );
+            Assert.Equal(0UL, node.Elipsis.Start );
+            Assert.Equal(5UL, node.Elipsis.End );
+        }
+        
+        [Fact]
+        public void TestNoneAtom()
+        {
+            var parser = Setup( new Token[] { new Token(0, 5, Token.TokenKind.PyNone ), new Token(5, 5, Token.TokenKind.EOF) } );
+            
+            Assert.True(parser != null);
+            NoneExpression node = (NoneExpression)parser.ParseAtom();
+            Assert.Equal(Token.TokenKind.PyNone, node.Operator.Kind );
+            Assert.Equal(0UL, node.Start );
+            Assert.Equal(5UL, node.End );
+            Assert.Equal(0UL, node.Operator.Start );
+            Assert.Equal(5UL, node.Operator.End );
+        }
+        
+        [Fact]
+        public void TestTrueAtom()
+        {
+            var parser = Setup( new Token[] { new Token(0, 5, Token.TokenKind.PyTrue ), new Token(5, 5, Token.TokenKind.EOF) } );
+            
+            Assert.True(parser != null);
+            TrueLiteralExpression node = (TrueLiteralExpression)parser.ParseAtom();
+            Assert.Equal(Token.TokenKind.PyTrue, node.Operator.Kind );
+            Assert.Equal(0UL, node.Start );
+            Assert.Equal(5UL, node.End );
+            Assert.Equal(0UL, node.Operator.Start );
+            Assert.Equal(5UL, node.Operator.End );
+        }
+        
+        [Fact]
+        public void TestFalseAtom()
+        {
+            var parser = Setup( new Token[] { new Token(0, 5, Token.TokenKind.PyFalse ), new Token(5, 5, Token.TokenKind.EOF) } );
+            
+            Assert.True(parser != null);
+            FalseLiteralExpression node = (FalseLiteralExpression)parser.ParseAtom();
+            Assert.Equal(Token.TokenKind.PyFalse, node.Operator.Kind );
+            Assert.Equal(0UL, node.Start );
+            Assert.Equal(5UL, node.End );
+            Assert.Equal(0UL, node.Operator.Start );
+            Assert.Equal(5UL, node.Operator.End );
+        }
+        
+        [Fact]
+        public void TestStringSingleAtom()
+        {
+            var parser = Setup( new Token[] { new Token(0, 5, Token.TokenKind.String ), new Token(5, 5, Token.TokenKind.EOF) } );
+            
+            Assert.True(parser != null);
+            StringLiteralExpression node = (StringLiteralExpression)parser.ParseAtom();
+            Assert.Equal(Token.TokenKind.String, node.Strings[0].Kind );
+            Assert.Equal(0UL, node.Start );
+            Assert.Equal(5UL, node.End );
+            Assert.Equal(0UL, node.Strings[0].Start );
+            Assert.Equal(5UL, node.Strings[0].End );
+        }
+        
+        [Fact]
+        public void TestStringMultipleAtom()
+        {
+            var parser = Setup( new Token[]
+            {
+                new Token(0, 5, Token.TokenKind.String ), 
+                new Token(10, 15, Token.TokenKind.String ), 
+                new Token(20, 25, Token.TokenKind.String ), 
+                new Token(26, 26, Token.TokenKind.EOF)
+            } );
+            
+            Assert.True(parser != null);
+            StringLiteralExpression node = (StringLiteralExpression)parser.ParseAtom();
+            Assert.Equal(3, node.Strings.Length);
+            Assert.Equal(Token.TokenKind.String, node.Strings[0].Kind );
+            Assert.Equal(0UL, node.Start );
+            Assert.Equal(26UL, node.End );
+            Assert.Equal(0UL, node.Strings[0].Start );
+            Assert.Equal(5UL, node.Strings[0].End );
+            Assert.Equal(Token.TokenKind.String, node.Strings[1].Kind );
+            Assert.Equal(10UL, node.Strings[1].Start );
+            Assert.Equal(15UL, node.Strings[1].End );
+            Assert.Equal(Token.TokenKind.String, node.Strings[2].Kind );
+            Assert.Equal(20UL, node.Strings[2].Start );
+            Assert.Equal(25UL, node.Strings[2].End );
         }
     }
 }
