053553160922939b206c58b401dbab0399199810
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-10-02 23:47:39 +0200

    Added node for all Expression List types.

---
+++ PythonCodeAnalyzer/Parser/Ast/Expression/ListExpression.cs
@@ -0,0 +1,27 @@
+﻿namespace PythonCodeAnalyzer.Parser.Ast.Expression
+{
+    public partial class ListExpression : ExpressionNode
+    {
+        public enum ListType
+        {
+            Unknown,
+            ExprList,
+            TestList,
+            SubscriptList,
+            ArgumentList,
+            SetEntries,
+            DictionaryEntries
+        }
+
+        public ListType ContainerType { get; set; }
+        public ExpressionNode[] Elements { get; set; }
+        public Token[] Separators { get; set; }
+        
+        public ListExpression(uint start, uint end, ListType type, ExpressionNode[] elements, Token[] separators) : base(start, end)
+        {
+            ContainerType = type;
+            Elements = elements;
+            Separators = separators;
+        }
+    }
+}
