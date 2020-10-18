6354c689bfd763234b3b4ce867fcf46e593146fc
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-10-18 17:35:33 +0200

    Added Unittest for await name with no call trailing.

--- TestPythonCodeAnalyzer/TestParser.cs
+++ TestPythonCodeAnalyzer/TestParser.cs
@@ -239,5 +239,30 @@ namespace TestPythonCodeAnalyzer
             Assert.Equal(0UL, node.Start );
             Assert.Equal(14UL, node.End );
         }
+        
+        [Fact]
+        public void TestAtomExprAwaitAndCall()
+        {
+            var parser = new PythonParser();
+            Assert.True(parser != null);
+            parser.Tokenizer = new PythonTokenizer("await __init__();".ToCharArray(), false, 8);
+            parser.Tokenizer.Advance();
+            
+            var node = (AtomExpression)parser.ParseAtomExpression();
+            Assert.True(node.IsAwait);
+            Assert.Equal(Token.TokenKind.PyAwait, node.Operator.Kind);
+            Assert.Equal(Token.TokenKind.Name, ((NameLiteralExpression)node.Right).Name.Kind);
+            Assert.Equal(6U, ((NameLiteralExpression)node.Right).Name.Start);
+            Assert.Equal(14U, ((NameLiteralExpression)node.Right).Name.End);
+            Assert.True(node.TrailerCollection != null);
+            Assert.Equal(0UL, node.Start );
+            Assert.Equal(16UL, node.End );
+
+            var trailerNode = node.TrailerCollection;
+            Assert.True(trailerNode.Length == 1);
+            Assert.Equal(Token.TokenKind.PyLeftParen, ((CallExpression) trailerNode[0]).Operator1.Kind);
+            Assert.True(((CallExpression) trailerNode[0]).Right == null);
+            Assert.Equal(Token.TokenKind.PyRightParen, ((CallExpression) trailerNode[0]).Operator2.Kind);
+        }
     }
 }
