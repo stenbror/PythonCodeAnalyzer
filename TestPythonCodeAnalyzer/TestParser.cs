628b2678048e86a63792eda34fb5fdf065fcce73
ricste <richard.magnor.stenbro@bisnode.com>
2020-10-27 11:20:50 +0100

    Added UnitTest for Trailer.

--- TestPythonCodeAnalyzer/TestParser.cs
+++ TestPythonCodeAnalyzer/TestParser.cs
@@ -2773,5 +2773,30 @@ namespace TestPythonCodeAnalyzer
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestTrailerMissingDotName()
+        {
+            var parser = new PythonParser();
+            Assert.True(parser != null);
+            parser.Tokenizer = new PythonTokenizer("a.; ".ToCharArray(), false, 8);
+            parser.Tokenizer.Advance();
+
+            try
+            {
+                parser.ParseNamedExpr();
+                Assert.True(false);
+            }
+            catch (SyntaxErrorException e)
+            {
+                Assert.Equal(2U, e.Position);
+                Assert.Equal(Token.TokenKind.PySemiColon, e.ErrorSymbol.Kind);
+                Assert.Equal("Expecting name literal after '.'", e.Message);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     }
 }
