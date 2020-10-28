af403854df0047d29f4afef461576093456d732a
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-10-28 19:30:54 +0100

    Added UnitTest for PlusAssign expression.

--- TestPythonCodeAnalyzer/TestParser.cs
+++ TestPythonCodeAnalyzer/TestParser.cs
@@ -4642,5 +4642,57 @@ namespace TestPythonCodeAnalyzer
             Assert.True(node.Separators.Length == 0);
             Assert.Equal(Token.TokenKind.Newline, node.NewLine.Kind);
         }
+        
+        [Fact]
+        public void TestExpressionStatementShiftRightAssign()
+        {
+            var parser = new PythonParser();
+            Assert.True(parser != null);
+            parser.Tokenizer = new PythonTokenizer("a >>= b\r\n\0".ToCharArray(), false, 8);
+            parser.Tokenizer.Advance();
+            
+            var node = (ListStatement) parser.ParseStmt();
+            Assert.True(node.Elements.Length == 1);
+            
+            var node2 = (AugAssignStatement) node.Elements[0];
+            Assert.Equal(AugAssignStatement.OperatorKind.ShiftRightAssign, node2.Kind);
+            Assert.Equal(0UL, node2.Start);
+            Assert.Equal(7UL, node2.End);
+
+            var left = (NameLiteralExpression)node2.Left;
+            Assert.Equal("a", left.Name.Text);
+            Assert.Equal(Token.TokenKind.PyShiftRightAssign, node2.Operator.Kind);
+            var right = (NameLiteralExpression) node2.Right;
+            Assert.Equal("b", right.Name.Text);
+            
+            Assert.True(node.Separators.Length == 0);
+            Assert.Equal(Token.TokenKind.Newline, node.NewLine.Kind);
+        }
+        
+        [Fact]
+        public void TestExpressionStatementShiftRightAssignYieldExpr()
+        {
+            var parser = new PythonParser();
+            Assert.True(parser != null);
+            parser.Tokenizer = new PythonTokenizer("a >>= yield b\r\n\0".ToCharArray(), false, 8);
+            parser.Tokenizer.Advance();
+            
+            var node = (ListStatement) parser.ParseStmt();
+            Assert.True(node.Elements.Length == 1);
+            
+            var node2 = (AugAssignStatement) node.Elements[0];
+            Assert.Equal(AugAssignStatement.OperatorKind.ShiftRightAssign, node2.Kind);
+            Assert.Equal(0UL, node2.Start);
+            Assert.Equal(13UL, node2.End);
+
+            var left = (NameLiteralExpression)node2.Left;
+            Assert.Equal("a", left.Name.Text);
+            Assert.Equal(Token.TokenKind.PyShiftRightAssign, node2.Operator.Kind);
+            var right = (YieldExpression) node2.Right;
+            Assert.Equal(Token.TokenKind.PyYield, right.Operator1.Kind);
+            
+            Assert.True(node.Separators.Length == 0);
+            Assert.Equal(Token.TokenKind.Newline, node.NewLine.Kind);
+        }
     }
 }
