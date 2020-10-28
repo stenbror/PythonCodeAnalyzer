8db29a2fc6f293c110e5a679855083aed300928a
ricste <richard.magnor.stenbro@bisnode.com>
2020-10-28 12:54:24 +0100

    Added UnitTest for import statement.

--- TestPythonCodeAnalyzer/TestParser.cs
+++ TestPythonCodeAnalyzer/TestParser.cs
@@ -3621,5 +3621,35 @@ namespace TestPythonCodeAnalyzer
             
             Assert.Equal(Token.TokenKind.Newline, node.NewLine.Kind);
         }
+        
+        [Fact]
+        public void TestImportStmt2()
+        {
+            var parser = new PythonParser();
+            Assert.True(parser != null);
+            parser.Tokenizer = new PythonTokenizer("import a.b\r\n\0".ToCharArray(), false, 8);
+            parser.Tokenizer.Advance();
+            
+            var node = (ListStatement) parser.ParseStmt();
+            Assert.True(node.Elements.Length == 1);
+            
+            var node2 = (ImportStatement) node.Elements[0];
+            Assert.Equal(Token.TokenKind.PyImport, node2.Operaor.Kind);
+            Assert.Equal(0UL, node2.Start);
+            Assert.Equal(10UL, node2.End);
+            
+            var node3 = (ImportListStatement)node2.Right;
+            Assert.True(node3.Elements.Length == 1);
+
+            var node4 = (DottedNameStatement)node3.Elements[0];
+            Assert.True(node4.Names.Length == 2);
+            Assert.True(node4.Dots.Length == 1);
+            Assert.Equal("a", node4.Names[0].Text);
+            Assert.Equal("b", node4.Names[1].Text);
+                            
+            Assert.True(node.Separators.Length == 0);
+            
+            Assert.Equal(Token.TokenKind.Newline, node.NewLine.Kind);
+        }
     }
 }
