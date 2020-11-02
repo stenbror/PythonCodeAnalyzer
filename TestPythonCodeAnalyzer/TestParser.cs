925384731f015e8ccea229cbecd689a9eddb5d4a
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-11-02 22:08:23 +0100

    Added UnitTest for VarArgsList rule.

--- TestPythonCodeAnalyzer/TestParser.cs
+++ TestPythonCodeAnalyzer/TestParser.cs
@@ -7459,5 +7459,29 @@ namespace TestPythonCodeAnalyzer
             var node4 = (VFPDefExpression)node.Elements[2];
             Assert.Equal("c", node4.Name.Text);
         }
+        
+        [Fact]
+        public void TestVarArgsList17()
+        {
+            var parser = new PythonParser();
+            Assert.True(parser != null);
+            parser.Tokenizer = new PythonTokenizer("a, b = 4:\r\n\r\n\0".ToCharArray(), false, 8);
+            parser.Tokenizer.Advance();
+            
+            var node = (VarArgsListExpression) parser.ParseVarArgsList();
+            Assert.Equal(0UL, node.Start);
+            Assert.Equal(8UL, node.End);
+
+            Assert.True(node.Elements.Length == 2);
+            Assert.True(node.Separators.Length == 1);
+            Assert.True(node.Div == null);
+            
+            var node2 = (VFPDefExpression)node.Elements[0];
+            Assert.Equal("a", node2.Name.Text);
+            
+            var node3 = (VarArgumentExpression)node.Elements[1];
+            Assert.Equal("b", ((VFPDefExpression)node3.Left).Name.Text);
+            Assert.Equal("4", ((NumberLiteralExpression)node3.Right).Number.Text);
+        }
     }
 }
