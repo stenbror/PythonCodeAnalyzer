c79fdade09e5a21986dc3a5cac9549543d954e7a
Richard Magnor Stenbro <stenbror@hotmail.com>
2020-11-11 19:09:01 +0100

    Added UnitTest for failing of for statement.

--- TestPythonCodeAnalyzer/TestParser.cs
+++ TestPythonCodeAnalyzer/TestParser.cs
@@ -8892,5 +8892,29 @@ namespace TestPythonCodeAnalyzer
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestCompoundStatementFailingForMissingColon()
+        {
+            var parser = new PythonParser();
+            Assert.True(parser != null);
+            parser.Tokenizer = new PythonTokenizer("for a in b pass\0".ToCharArray(), false, 8);
+            
+            try
+            {
+                parser.ParseFileInput();
+                Assert.True(false);
+            }
+            catch (SyntaxErrorException e)
+            {
+                Assert.Equal(11U, e.Position);
+                Assert.Equal(Token.TokenKind.PyPass, e.ErrorSymbol.Kind);
+                Assert.Equal("Expecting ':' in for statement!", e.Message);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     } 
 }
